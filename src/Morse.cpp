14a0576f8e5e526963b7752b8e2fb9bd5429a806
Igorello74 <sidneff.igor@ya.ru>
2019-11-23 22:16:17 +0500

    replace tabs with double space

--- src/Morse.cpp
+++ src/Morse.cpp
@@ -16,81 +16,81 @@ Morse::Morse(byte pin, byte unit, MDebug debug = NONE) {
 }
 
 void Morse::play (MSymb ser[], byte num) {
-	for (byte i = 0; i < num; i++) {
-		switch (ser[i]) {
-			case DO:
-				_dot();
-				break;
-			case DA:
-				_dash();
-				break;
-			case PS:
-				_pspace();
-				break;
-			case LS:
-				_lspace();
-				break;
-			case WS:
-				_wspace();
-				break;
-		}
-	}
+  for (byte i = 0; i < num; i++) {
+    switch (ser[i]) {
+      case DO:
+        _dot();
+        break;
+      case DA:
+        _dash();
+        break;
+      case PS:
+        _pspace();
+        break;
+      case LS:
+        _lspace();
+        break;
+      case WS:
+        _wspace();
+        break;
+    }
+  }
 }
 
 void Morse::play (String &ser) {
-	for(byte i = 0; i < ser.length(); i++) {
-		if (ser[i] == '.' or ser[i] == '*') _dot();
-		else if (ser[i] == '-')	_dash();
-		else if (ser[i] == ' ')	_lspace();
-		else if (ser[i] == '/')	_wspace();
+  for(byte i = 0; i < ser.length(); i++) {
+    if (ser[i] == '.' or ser[i] == '*') _dot();
+    else if (ser[i] == '-')  _dash();
+    else if (ser[i] == ' ')  _lspace();
+    else if (ser[i] == '/')  _wspace();
 
-		if ((i != ser.length()-1) and (ser[i+1] != '/') and (ser[i+1] != ' ')
-		and (ser[i] != '/') and (ser[i] != ' '))	_pspace();
-	}
-	
+    if ((i != ser.length()-1) and (ser[i+1] != '/') and (ser[i+1] != ' ')
+    and (ser[i] != '/') and (ser[i] != ' '))  _pspace();
+  }
+  
 }
 
 void Morse::setUnit (byte unit) {
-	if (_debug > LED) Serial.println("Set unit: " + String(_unit) + "-->" \
-	+ String(unit));
-	_unit=unit;
+  if (_debug > LED) Serial.println("Set unit: " + String(_unit) + "-->" \
+  + String(unit));
+  _unit=unit;
 }
 
 void Morse::_dot() {
-	if (_debug > LED) Serial.println("DO");
-	if (_debug == LED or _debug == SERLED) digitalWrite(13, HIGH);
-	digitalWrite(_pin, HIGH);
-	delay(_unit);
-	if (_debug == LED or _debug == SERLED) digitalWrite(13, LOW);
-	digitalWrite(_pin, LOW);
+  if (_debug > LED) Serial.println("DO");
+  if (_debug == LED or _debug == SERLED) digitalWrite(13, HIGH);
+  digitalWrite(_pin, HIGH);
+  delay(_unit);
+  if (_debug == LED or _debug == SERLED) digitalWrite(13, LOW);
+  digitalWrite(_pin, LOW);
 }
 
 void Morse::_dash() {
-	if (_debug > LED) Serial.println("DA");
-	if (_debug == LED or _debug == SERLED) digitalWrite(13, HIGH);
-	digitalWrite(_pin, HIGH);
-	delay(_unit*3);
-	if (_debug == LED or _debug == SERLED) digitalWrite(13, LOW);
-	digitalWrite(_pin, LOW);
+  if (_debug > LED) Serial.println("DA");
+  if (_debug == LED or _debug == SERLED) digitalWrite(13, HIGH);
+  digitalWrite(_pin, HIGH);
+  delay(_unit*3);
+  if (_debug == LED or _debug == SERLED) digitalWrite(13, LOW);
+  digitalWrite(_pin, LOW);
 }
 
 void Morse::_pspace() {
-	if (_debug > LED) Serial.println("PS");
-	if (_debug == LED or _debug == SERLED) digitalWrite(13, LOW);
-	digitalWrite(_pin, LOW);
-	delay(_unit);
+  if (_debug > LED) Serial.println("PS");
+  if (_debug == LED or _debug == SERLED) digitalWrite(13, LOW);
+  digitalWrite(_pin, LOW);
+  delay(_unit);
 }
 
 void Morse::_lspace() {
-	if (_debug > LED) Serial.println("LS");
-	if (_debug == LED or _debug == SERLED) digitalWrite(13, LOW);
-	digitalWrite(_pin, LOW);
-	delay(_unit*3);
+  if (_debug > LED) Serial.println("LS");
+  if (_debug == LED or _debug == SERLED) digitalWrite(13, LOW);
+  digitalWrite(_pin, LOW);
+  delay(_unit*3);
 }
 
 void Morse::_wspace() {
-	if (_debug > LED) Serial.println("WS");
-	if (_debug == LED or _debug == SERLED) digitalWrite(13, LOW);
-	digitalWrite(_pin, LOW);
-	delay(_unit*7);
+  if (_debug > LED) Serial.println("WS");
+  if (_debug == LED or _debug == SERLED) digitalWrite(13, LOW);
+  digitalWrite(_pin, LOW);
+  delay(_unit*7);
 }
