8b31004bcf47818fd8e88e03cb6a0897d02b6b52
Igorello74 <sidneff.igor@ya.ru>
2019-11-23 23:14:29 +0500

    Update and rename SOS.ino to SOS_withArray.ino

---
+++ examples/SOS/SOS_withArray.ino
@@ -0,0 +1,14 @@
+#include <Morse.h>
+
+MSer seq[] = {DO, PS, DO, PS, DO, PS, DA, PS, DA, PS, DA, PS, DO, PS, DO, PS, DO, WS}; // create a sequence to play
+
+Morse morser(12, 50); 
+// create an instance of the Morse class, 12 is the pin to which executing device is connected, 50 is the duration of a unit (in ms)
+
+void setup() {
+  // we don't need to call pinMode, all setting are done in the constructor
+  }
+
+void loop() {
+	morser.play(seq, sizeof(seq)); // play the morse sequnce we saved above. The second argument is the number of members in the array
+}
